0531e0a1213916832bec2dbf10c3578bbebd88b5
mw-beep <mw-beep@users.noreply.github.com>
2026-05-20 18:46:45 +0100

    wrote extract_text_from_pdf function

---
+++ extractor.py
@@ -0,0 +1,18 @@
+import pdfplumber
+
+
+def extract_text_from_pdf(pdf_file):
+
+    with pdfplumber.open(pdf_path) as pdf:
+
+        extracted_text = []
+        for page_number, page in enumerate(pdf.pages, start=1):
+            text = page.extract_text()
+            if text is None:
+               print (f"no text found on {page_number}")
+            else:
+                extracted_text.append(text)
+
+
+        full_text = "\n".join(extracted_text)
+    return full_text
